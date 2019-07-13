fcd5f4b88d971f15acb0a74af916a414d9614111
Ebzonman <ebzonman@gmail.com>
2019-07-13 16:01:35 +1000

    pushhhshshs

--- routes.py
+++ routes.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, url_for, request, redirect
-from server import app #, system
+from server import app, reminders
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     return render_template("home.html")
+
+@app.route("/calendar", methods=["GET", "POST"])
+def calendar():
+    return render_template("calendar.html", reminders=reminders)
--- server.py
+++ server.py
@@ -3,6 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 app.secret_key = "secret-sause"
 
+reminders = []
+
 # Sets up global filters for the Jinja2 environment
 # @app.template_test('patient')
 
